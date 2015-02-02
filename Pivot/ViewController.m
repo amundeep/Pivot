5b7776cf315e06b1bf739a01f368befd8fe755d6
Amundeep Singh <amundeep@gmail.com>
2015-02-01 23:51:59 -0800

    init
    Initial commit

--- Pivot/ViewController.m
+++ Pivot/ViewController.m
@@ -7,12 +7,15 @@
 //
 
 #import "ViewController.h"
+#import <AVFoundation/AVFoundation.h>
 
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
+
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
